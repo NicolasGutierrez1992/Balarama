0dc235e3495157ad44dcf2bb36b178c36f1b0cdd
NicolasGutierrez1992 <nicolasgutierrez10492@gmail.com>
2024-01-08 23:06:19 -0300

    agrego busqueda de precio

--- scr/routes/index.js
+++ scr/routes/index.js
@@ -5,11 +5,15 @@ const {createTableWeb,truncateArticulosWeb,setArticulosWeb,getArticulos, setArti
 
 router.get("/update/:codebar",getArticulos);//Trae el precio de del producto
 router.get("/update",getArticulos);//Trae los precios almacenados en la tabla web
+
 router.post("/update",setArticulos);//Guarda la lista de precios local en la web
 router.post("/updateWeb",setArticulosWeb);//Guarda el precio nuevo en tabla Web
+
 router.get("/ping",ping);//Prueba conexion
+
 router.get("/create",createTable);//Crea la tabla para la lista de precios local
 router.get("/createWeb",createTableWeb);//Crea la tabla web
+
 router.get("/truncate",truncateArticulos);//vacia la lista de precios local
 router.get("/truncateWeb",truncateArticulosWeb);//vacia la tabla web
 
