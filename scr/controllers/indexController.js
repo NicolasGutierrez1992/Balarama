271a7f0d0e073cf2dbaad0438f58a34602deebd3
NicolasGutierrez1992 <nicolasgutierrez10492@gmail.com>
2024-01-08 23:00:20 -0300

    agrego busqueda de precio

--- scr/controllers/indexController.js
+++ scr/controllers/indexController.js
@@ -28,8 +28,8 @@ const getArticulos = async (req,res)=>{
         console.log("query " + query);
         response =  await pool.query(query);
       }else{
-        console.log("consulto update 1 " );
-        response =  await pool.query("select * from ArticulosPG where Update = 1");
+        console.log("consulto tabla ArticulosWebPG " );
+        response =  await pool.query("select * from ArticulosWebPG ");
       }
       
       console.log("Devuelvo ");
@@ -57,72 +57,83 @@ const setArticulos = async (req,res)=>{
   }
 
 }
-  
-  const clearArticulos = async (req,res)=>{
-    try {
-        console.log("Entra a clearArticulos");
-        const query = "Update ArticulosPG SET Update = 0 ;";
-        console.log(query);
-        const response = await pool.query(query);
-        res.status(200).json("ARTICULOS ACTUALIZADOS");
-    } catch (error) {
-        res.status(500).json(error);
-    }
+const setArticulosWeb = async (req,res)=>{
+   
+  try {
+      console.log("Entra a setArticulosWeb");
+      let articulo = {...req.body}
+      console.log(articulo);
+      const query = "INSERT INTO ArticulosWebPG (CodBar,Nombre,Precio,Update) VALUES ( '" + articulo.Codbar +"','"+ articulo.Nombre +"',"+ articulo.Precio +","+ articulo.Update + ");";
+      console.log(query);
+      const response = await pool.query(query);
+      res.status(200).json("ARTICULO AGREGADO");  
+  } catch (error) {
+      res.status(500).json(error);
   }
 
-  const truncateArticulos = async (req,res)=>{
-    try {
-        console.log("Entra a truncateArticulos");
-        const query = "truncate table ArticulosPG;";
-        console.log(query);
-        const response = await pool.query(query);
-        res.status(200).json("ARTICULOS ELIMINADOS");
-    } catch (error) {
-        res.status(500).json(error);
-    }
+}
+ 
+const truncateArticulos = async (req,res)=>{
+  try {
+      console.log("Entra a truncateArticulos");
+      const query = "truncate table ArticulosPG;";
+      console.log(query);
+      const response = await pool.query(query);
+      res.status(200).json("ARTICULOS ELIMINADOS");
+  } catch (error) {
+      res.status(500).json(error);
   }
+} 
+const truncateArticulosWeb = async (req,res)=>{
+  try {
+      console.log("Entra a truncateArticulosWeb");
+      const query = "truncate table ArticulosWebPG;";
+      console.log(query);
+      const response = await pool.query(query);
+      res.status(200).json("ARTICULOS ELIMINADOS");
+  } catch (error) {
+      res.status(500).json(error);
+  }
+}
 
-  const ping = async (req,res)=>{
-    try{
-        const query = "SELECT NOW();";
-        const response = await pool.query(query);
-          res.status(200).json(response.rows[0]);
-    }catch(err){
-        res.status(500).json(err);
-    }
-  }
-  
-    
-  const createTable = async (req,res)=>{
-    try{
-        const query = "CREATE TABLE ArticulosPG( CodBar  varchar(40), Nombre varchar(40),  Precio float, Update int);";
-        const response = await pool.query(query);
+const ping = async (req,res)=>{
+  try{
+      const query = "SELECT NOW();";
+      const response = await pool.query(query);
         res.status(200).json(response.rows[0]);
-    }catch(err){
-        res.status(500).json(err);
-    }
+  }catch(err){
+      res.status(500).json(err);
   }
+}
 
-  const updateArticulos = async ( req, res)=>{
-    try {
-        console.log("Entra a updateArticulos");
-        let articulo = {...req.body}
-        console.log(articulo);
-        const query = "update Articulos SET Precio = '"+articulo.Precio+"' , Nombre = '" + articulo.Nombre+"');";
-        console.log(query);
-        const response = await pool.query(query);
-        res.status(200).json("ARTICULO ACTUALIZADO");
-    } catch (err) {
-       res.status(500).json(err);
-    }
+  
+const createTable = async (req,res)=>{
+  try{
+      const query = "CREATE TABLE ArticulosPG( CodBar  varchar(40), Nombre varchar(40),  Precio float, Update int);";
+      const response = await pool.query(query);
+      res.status(200).json(response.rows[0]);
+  }catch(err){
+      res.status(500).json(err);
   }
+}
+const createTableWeb = async (req,res)=>{
+  try{
+      const query = "CREATE TABLE ArticulosWebPG( CodBar  varchar(40), Nombre varchar(40),  Precio float, Update int);";
+      const response = await pool.query(query);
+      res.status(200).json(response.rows[0]);
+  }catch(err){
+      res.status(500).json(err);
+  }
+}
+
 
 module.exports={
+    truncateArticulosWeb,
     getArticulos,
     setArticulos,
-    clearArticulos,
     ping,
     createTable,
-    updateArticulos,
-    truncateArticulos
+    createTableWeb,
+    truncateArticulos,
+    setArticulosWeb
   }
--- scr/routes/index.js
+++ scr/routes/index.js
@@ -1,14 +1,16 @@
 const{Router} = require('express');
 const router = Router();
-const {getArticulos, setArticulos,clearArticulos,ping,createTable,updateArticulos,truncateArticulos} = require('../controllers/indexController')
+const {createTableWeb,truncateArticulosWeb,setArticulosWeb,getArticulos, setArticulos,ping,createTable,truncateArticulos} = require('../controllers/indexController')
 
 
-router.get("/update/:codebar",getArticulos);
-router.get("/update",getArticulos);
-router.post("/update",setArticulos);
-router.get("/clear",clearArticulos);
-router.get("/ping",ping);
-router.get("/create",createTable);
-router.get("/truncate",truncateArticulos);
-router.get("/updateArticulos",updateArticulos);
+router.get("/update/:codebar",getArticulos);//Trae el precio de del producto
+router.get("/update",getArticulos);//Trae los precios almacenados en la tabla web
+router.post("/update",setArticulos);//Guarda la lista de precios local en la web
+router.post("/updateWeb",setArticulosWeb);//Guarda el precio nuevo en tabla Web
+router.get("/ping",ping);//Prueba conexion
+router.get("/create",createTable);//Crea la tabla para la lista de precios local
+router.get("/createWeb",createTableWeb);//Crea la tabla web
+router.get("/truncate",truncateArticulos);//vacia la lista de precios local
+router.get("/truncateWeb",truncateArticulosWeb);//vacia la tabla web
+
 module.exports = router;
